8c69178e5587aff1350a7d06ace2f954e56942ca
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-06-19 22:43:52 -0700

    UV index API working

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,30 @@
+//global lattitude and longitude variables
+let lat;
+let lon;
+
+//get UV data
+let getUv = () => {
+    let urlKey = `http://api.openweathermap.org/data/2.5/uvi?appid=19b6b8e66066ceceeddf41a7b1b7f8b3&lat=${lat}&lon=${lon}`
+    $.ajax({
+        url: urlKey, 
+        method: 'GET'
+    }).then(function(response){
+        $('#current-weather').append($('<div id="index-div">').text(`UV Index:`));
+        $('#index-div').append($(`<div id="index">`).text(response.value));
+    })
+}
+
+//get 5 day forecast
+// let getForecast = (cityName) => {
+//     let urlKey = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&cnt=5&appid=19b6b8e66066ceceeddf41a7b1b7f8b3&units=imperial`
+//     $.ajax({
+//         url: urlKey, 
+//         method: 'GET'
+//     }).then(function(response){
+//         console.log(response);
+        
+//         })
+// }
 
 // get current weather
 let getCurrent = (cityName) => {
@@ -7,18 +34,25 @@ let getCurrent = (cityName) => {
         method: 'GET'
     }).then(function(response){
         console.log(response);
-        $('#current-weather').append($('<p>').text(`Temperature: ${response.main.temp} \xB0F`));
-        $('#current-weather').append($('<p>').text(`Humidity: ${response.main.humidity}%`));
-        $('#current-weather').append($('<p>').text(`Wind speed: ${response.wind.speed} MPH`));
-        })
+        lat = response.coord.lat;
+        lon = response.coord.lon;
+        let cityTitle = $('<h2>').text(`${cityName}(${moment().format('MM')}/${moment().format('DD')}/${moment().format('YYYY')})`);
+        $('#current-weather').append(cityTitle);
+        let icon = $(`<img src="http://openweathermap.org/img/w/${response.weather[0].icon}.png">`);
+        $('#current-weather').append(icon);
+        $('#current-weather').append($('<div>').text(`Temperature: ${response.main.temp} \xB0F`));
+        $('#current-weather').append($('<div>').text(`Humidity: ${response.main.humidity}%`));
+        $('#current-weather').append($('<div>').text(`Wind speed: ${response.wind.speed} MPH`));
+        getUv();
+        // getForecast();    
+    })
+            
 }
 
 // Listen to search key
 $('#search').click((event)=>{
     event.preventDefault();
-    let newCity = $('#city-search').val()
-    $('#city-search').val('')
+    let newCity = $('#city-search').val();
+    $('#city-search').val('');
     getCurrent(newCity);
-    let cityTitle = $('<h2>').text(`${newCity}`)
-    $('#current-weather').append(cityTitle)
 })
